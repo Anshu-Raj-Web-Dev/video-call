5851e58bea42ee824b40ee089f46d940a0f69db6
Anshu-Raj-Web-Dev <157576222+Anshu-Raj-Web-Dev@users.noreply.github.com>
2025-03-05 20:30:34 +0530

    Update VideoPage.js

--- src/components/VideoPage.js
+++ src/components/VideoPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
 import { APP_ID, SERVER_SECRET } from './constant'
-import "./VideoPage.css"
+import "./VideoPage.css" 
 
 const VideoPage = () => {
     const {id} = useParams()
